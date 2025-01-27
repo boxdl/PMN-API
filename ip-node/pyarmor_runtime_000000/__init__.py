f9dcf831c91ae572906e5f93b8b55e434d35ac44
boxdl <83626487+boxdl@users.noreply.github.com>
2025-01-27 08:10:07 +0000

    修复了不能运行的错误

--- ip-node/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.0.7 (trial), 000000, 2025-01-27T15:37:07.538111
-from .pyarmor_runtime import __pyarmor__
--- node/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.0.7 (trial), 000000, 2025-01-17T10:07:58.154341
-from .pyarmor_runtime import __pyarmor__
